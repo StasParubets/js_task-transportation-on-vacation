fb229f0d84ddd058fc29b91a8318bb6f083a974e
Stanislav <parustas@gmail.com>
2026-02-28 09:43:34 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -13,7 +13,9 @@ function calculateRentalCost(days) {
 
   if (days >= LONG_TERM) {
     return price - LONG_TERM_DISCOUNT;
-  } else if (days >= SHORT_TERM) {
+  }
+
+  if (days >= SHORT_TERM) {
     return price - SHORT_TERM_DISCOUNT;
   }
 
